15455951995895b145565bbd4d5f4b46c32280f8
JIN <bgm004@qq.com>
2023-12-16 16:52:19 +0800

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require("express")();
+
+app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`<h1>dpqb</h1>`);
+});
+
+module.exports = app;
