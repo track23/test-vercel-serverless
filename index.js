05da237b2caf6406eb58e65d4aa5c833ebf6a671
JIN <bgm004@qq.com>
2023-12-16 17:16:14 +0800

    test2

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = require("express")();
 app.get("/", (req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`<h1>dpqb say hello word</h1>`);
+  res.end(`<h1>dpqb say hello word</h1><br/><h2>test2</h2>`);
+});
+
+app.get("/qpdb", (req, res) => {
+  const data = { name: "dqpb" };
+  res.json(data);
 });
 
 module.exports = app;
