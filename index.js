d92d16c371e75289acf619106298a8c55d6451c0
JIN <bgm004@qq.com>
2023-12-16 17:00:16 +0800

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require("express")();
 app.get("/", (req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`<h1>dpqb</h1>`);
+  res.end(`<h1>dpqb say hello word</h1>`);
 });
 
 module.exports = app;
